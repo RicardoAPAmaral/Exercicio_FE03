f6f360fb81d9d6fcf62af7d040f455e349d5bb6b
Ricardo <ricardoamaral.sistemas@gmail.com>
2024-05-03 07:51:14 -0300

    segundo commit

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import './index.css'
 import Home from './pages/Home'
 import Header from './components/Header'
 import Footer from './Footer/Footer'
+import SimpleCounter from './components/SimpleCounter'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
      <Header />
       <Home />
+      <Footer/>
    
   </React.StrictMode>,
 )
